60816b0e2b4d977e8a789fa0ccf3aa45e2e7a750
Abiduzzaman Abid <itisabidss@gmail.com>
2018-07-11 03:31:19 +0000

    classwork6

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
   var server = http.createServer(function(req, res){
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -7,3 +7,18 @@ var http = require('http');
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
+*/
+ var http = require("http");
+ var express =  require("http");
+ var app = express();
+ var server = http.Server(app);
+ 
+ app.get('/', function(req, res){
+     res.sendFile(__dirname+'/index.html');
+ 
+ });
+ 
+ server.listen(process.env.PORT, process.env.IP, function(){
+     console.log("server Running");
+     
+ });

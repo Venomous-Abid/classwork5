7ffc5b0a3fc1d16a09485f730bc7ff73abdc4187
Abiduzzaman Abid <itisabidss@gmail.com>
2018-07-25 03:35:59 +0000

    classwork-8

--- server.js
+++ server.js
@@ -9,14 +9,55 @@
   });
 */
  var http = require("http");
- var express =  require("http");
+ var express = require("express");
  var app = express();
+ 
  var server = http.Server(app);
+ var bodyParser = require('body-parser');
+ 
+ var mongo=require("mongodb");
+ var db, uri = "mongodb://"+"classwork5-venomousabid.c9users.io/email";
+ ///uri ="mongodb://"+"classwork5-venomousabid.c9users.io/email"
+
+ mongo.MongoClient.connect(uri, 
+    {useNewUrlParser: true}, 
+    function(err, client){
+        if(err){
+            console.log("could not connect to MongoDB");
+           } else {
+                db =client.db('simple-node');
+            }
+        }
+     
+ });
+ 
+ var save =function(form_dat){
+     db.createCollection('users',function(err,collection{}));
+     var collection=db.collection('users');
+     collection.save(form_data);
+ }
+ 
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended:false}));
+ 
  
  app.get('/', function(req, res){
      res.sendFile(__dirname+'/index.html');
  
  });
+ 
+ app.get('/email', function(req, res){
+     res.sendFile(__dirname+'/email.html');
+ 
+ });
+ 
+app.post('/submit_user', function(req, res) {
+ 
+   console.log(req.body);
+   save(req,body);
+   res.status('200');
+     
+});
  
  server.listen(process.env.PORT, process.env.IP, function(){
      console.log("server Running");
